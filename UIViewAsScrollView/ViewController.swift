92d7822d835b678cca4e8142b18e38fcc3b725de
Avijit Nagre <avijit.nagre@weatherford.com>
2018-12-03 13:38:20 +0530

    Initial Commit

---
+++ UIViewAsScrollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIViewAsScrollView
+//
+//  Created by Avijit Nagare on 03/12/18.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
